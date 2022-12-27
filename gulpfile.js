39760b022cef58a0cef7d9fa1f52a000507043df
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-28 00:08:25 +0700

    update task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,38 @@ const gulp = require('gulp');
 const sbg = require('static-blog-generator');
 //const sbg = require('./packages/gulp-sbg');
 
+/**
+ * git pull on deploy dir
+ */
 async function pull() {
   const config = sbg.getConfig();
-  const deployDir = config.deploy.deployDir;
+  const cwd = config.deploy.deployDir;
+  const gh = config.deploy.github;
+  await gh.spawn('git', ['config', 'pull.rebase', 'false'], {
+    cwd
+  });
+  await gh.spawn('git', ['pull', '-X', 'theirs'], { cwd, stdio: 'inherit' });
+  const submodules = await gh.submodule.get();
+  for (let i = 0; i < submodules.length; i++) {
+    const sub = submodules[i];
+
+    try {
+      await gh.spawn('git', ['config', 'pull.rebase', 'false'], {
+        cwd: sub.root
+      });
+    } catch (e) {
+      // console.log(e.message, sub.root);
+    }
+
+    try {
+      await gh.spawn('git', ['pull', '-X', 'theirs'], {
+        cwd: sub.root,
+        stdio: 'inherit'
+      });
+    } catch (e) {
+      console.log('cannot pull', sub.root);
+    }
+  }
 }
 
 exports.sbg = sbg;
