5d0fd11501cb5ee08535127b5249a50b6ce698bc
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-27 23:43:21 +0700

    update deps
    Former-commit-id: 785e708cb66d48be68b1e8d28505791825fbec2b

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,11 @@ const gulp = require('gulp');
 const sbg = require('static-blog-generator');
 //const sbg = require('./packages/gulp-sbg');
 
+async function pull() {
+  const config = sbg.getConfig();
+  const deployDir = config.deploy.deployDir;
+}
+
 exports.sbg = sbg;
+exports.pull = pull;
 exports = gulp;
