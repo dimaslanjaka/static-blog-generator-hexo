d84c61ca8a3a1108faca81ab3c0ef171f09cdae7
dimaslanjaka <dimaslanjaka@gmail.com>
2022-12-31 20:06:19 +0700

    update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sbg = require('static-blog-generator');
+const gch = require('git-command-helper');
 //const sbg = require('./packages/static-blog-generator');
 
 const config = sbg.getConfig();
@@ -44,7 +45,9 @@ async function commit() {
   const gh = config.deploy.github;
   const doCommit = async (cwd) => {
     await gh.spawn('git', ['add', '.'], { cwd });
-    // const urlCommit = await
+    const remote = await gch.ext.getGithubRemote('origin', { cwd });
+    const branch = await gch.ext.getGithubCurrentBranch({ cwd });
+    console.log(remote, branch);
   };
   doCommit(cwd);
   const submodules = await gh.submodule.get();
