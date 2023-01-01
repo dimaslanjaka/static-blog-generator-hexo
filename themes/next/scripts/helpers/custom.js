0f0374585cfad6c29d8bc4690df436ac886fc702
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-01 18:04:02 +0700

    update snippet and helper
    Former-commit-id: b3a8c63199763b4ce46eebdc292ce9cf9b05483d

--- themes/next/scripts/helpers/custom.js
+++ themes/next/scripts/helpers/custom.js
@@ -2,3 +2,11 @@ hexo.extend.helper.register('getPosts', function () {
   const { page } = this;
   return page.posts;
 });
+
+hexo.extend.helper.register('getAuthor', function (author, fallback) {
+  if (typeof author === 'string') return author;
+  if (!author) return fallback;
+  if (author.name) return author.name;
+  if (author.nick) return author.nick;
+  if (author.nickname) return author.nickname;
+});
