a6c9be31156d478baccc96f2ca31504d7144b707
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-01 20:04:57 +0700

    update hexo
    Former-commit-id: 53443ff55ca405203905b4984ac1fef1ea8ef50e

--- themes/next/scripts/helpers/custom.js
+++ themes/next/scripts/helpers/custom.js
@@ -1,3 +1,7 @@
+/* global hexo */
+/// <reference types="hexo" />
+/// <reference types="node" />
+
 hexo.extend.helper.register('getPosts', function () {
   const { page } = this;
   return page.posts;
@@ -9,4 +13,9 @@ hexo.extend.helper.register('getAuthor', function (author, fallback) {
   if (author.name) return author.name;
   if (author.nick) return author.nick;
   if (author.nickname) return author.nickname;
+});
+
+hexo.extend.helper.register('getPost', function (by) {
+  const hexo = this;
+  console.log(hexo)
 });
