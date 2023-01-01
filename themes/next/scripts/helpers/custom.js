4e7c82a796afb7c2dee775ed2d959fca97f4e320
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-01 23:05:45 +0700

    update script
    Former-commit-id: 222d44f608705e82108f00f7bc717e14d8f21a12

--- themes/next/scripts/helpers/custom.js
+++ themes/next/scripts/helpers/custom.js
@@ -16,7 +16,7 @@ hexo.extend.helper.register('getAuthor', function (author, fallback) {
 hexo.extend.helper.register(
   'getPost',
   /**
-   * get post by key with name
+   * hexo get post by key with name
    * @param {'tags'|'categories'} by
    * @param {string[]} filternames
    * @returns {Record<string, string>[]}
@@ -27,21 +27,30 @@ hexo.extend.helper.register(
      * @type {any[]}
      */
     const data = hexo.site[by].data;
-    const map = filternames.map((filtername) => {
-      const filter = data.filter(({ name }) => String(name).toLowerCase() == filtername.toLowerCase());
-      return filter.map((group) => {
-        return group.posts.map(
-          /**
-           * @param {import('hexo').Post.Data} post
-           */
-          function ({ title, permalink }) {
-            // do what you have to do with each post
-            return { title, permalink };
-          }
-        );
-      });
-    }).flat()
+    const map = filternames
+      .map((filtername) => {
+        const filter = data.filter(({ name }) => String(name).toLowerCase() == filtername.toLowerCase());
+        return filter.map((group) => {
+          return group.posts.map(
+            /**
+             * @param {import('hexo').Post.Data} post
+             */
+            function ({ title, permalink }) {
+              // get title and permalink
+              // for more keys, you can look at https://github.com/dimaslanjaka/nodejs-package-types/blob/ec9b509d81eefdfada79f1658ac02118936a1e5a/index.d.ts#L757-L762
+              return { title, permalink };
+            }
+          );
+        });
+      })
+      // flattern all multidimensional arrays
+      // to get array of hexo post object
+      .flat(2);
+    // dump
+    // console.log(map);
+    // return an JSON string
     // return JSON.stringify(map, null, 2);
+    // return an Array
     return map;
   }
 );
