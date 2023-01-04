75d867ef1d84113339c87452acee6511c23b1392
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-04 18:59:53 +0700

    remove contents
    Former-commit-id: 17e96d05e0b9b28a61192a1f65a6e321dda7e0ba

--- git-hooks/pre-push.js
+++ git-hooks/pre-push.js
@@ -1,14 +1,1 @@
-const path = require('path');
-
-process.cwd = () => path.resolve(__dirname + '/..');
-const glob = require('glob');
-
-const packages = path.resolve(process.cwd() + '/packages');
-
-const files = glob('**/*.{js,ts}', {
-  cwd: packages,
-  ignore: ['**/node_modules', '**/tmp', '**/dist', '**/build', '**/test'],
-  sync: true
-});
-
-console.log(files);
+console.log('pre-push hooks')
