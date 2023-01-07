c00363d137d89aa6c1e7ace627c64506ea4243ec
dimaslanjaka <dimaslanjaka@gmail.com>
2023-01-07 18:01:59 +0700

    test post copy

---
+++ gulpfile-copy.js
@@ -0,0 +1,29 @@
+const { Application, gulp } = require('./packages/static-blog-generator');
+
+function copy(done) {
+  const api = new Application(__dirname);
+  console.log('starting copy');
+  api
+    .copy()
+    .then(() => {
+      console.log('post copy done occurs');
+      done();
+    })
+    .catch(() => {
+      console.log('post copy error occurs');
+      done();
+    })
+    .finally(() => {
+      console.log('post copy finally occurs');
+      done();
+    });
+}
+
+gulp.task('start-copy', gulp.series(copy));
+
+if (require.main === module) {
+  //console.log('called directly');
+  copy();
+} else {
+  //console.log('required as a module');
+}
