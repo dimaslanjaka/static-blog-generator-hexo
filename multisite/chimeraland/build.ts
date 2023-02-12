fe83f9cb6111584002430fe4261978a15ccb4e60
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-12 21:00:01 +0700

    disable copy
    Former-commit-id: 5a652ee96aa69b74a5333669718c6c97c6bf1175

--- multisite/chimeraland/build.ts
+++ multisite/chimeraland/build.ts
@@ -1,10 +1,7 @@
-import { copyPost } from './project'
 import './src'
 import copySource from './src/copy-source'
 import copySrcPost from './src/copy-src-posts'
 
 copySrcPost().on('end', function () {
-  copySource().on('end', function () {
-    copyPost()
-  })
+  copySource()
 })
