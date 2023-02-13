773f79fb9f21fc684adb93197de4733ffb1b3560
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-02-13 18:57:56 +0700

    restore post copy
    Former-commit-id: 9808e09e11b7eca45c7bb3ec27c3cdc5bef5c60f

--- multisite/chimeraland/project.js
+++ multisite/chimeraland/project.js
@@ -1,17 +1,17 @@
 const { join, toUnix } = require('upath')
-// const spawn = require('cross-spawn')
+const spawn = require('cross-spawn')
 
 const hexoProject = join(__dirname, '../../')
 const chimeralandProject = toUnix(__dirname)
 
 function copyPost() {
   console.log('npm run copy on', hexoProject)
-  // spawn('npm', ['run', 'copy'], { cwd: hexoProject, stdio: 'inherit' })
+  spawn('npm', ['run', 'copy'], { cwd: hexoProject, stdio: 'inherit', shell: true })
 }
 
 function generateSite() {
   console.log('npm run build on', hexoProject)
-  // spawn('npm', ['run', 'build'], { cwd: hexoProject, stdio: 'inherit' })
+  spawn('npm', ['run', 'build'], { cwd: hexoProject, stdio: 'inherit', shell: true })
 }
 
 module.exports = {
