e2a6adf6a34611e4437b5189b629b9eb2fcb1456
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-15 15:26:34 +0700

    update agc
    Former-commit-id: b3a8fe74b9dd69021365c8ee6097697b301aa5c3

--- packages/agc/src/yt-search.runner.ts
+++ packages/agc/src/yt-search.runner.ts
@@ -1,3 +1,12 @@
-import { search_yt } from './yt-search';
+import Nightmare from 'nightmare';
+const nightmare = Nightmare({ show: true });
 
-search_yt('Chimeraland Serpenine');
+nightmare
+  .goto('https://www.youtube.com/results?search_query=chimeraland+level')
+  .wait('yt-formatted-string')
+  .evaluate(() => document.querySelector('yt-formatted-string').textContent)
+  .end()
+  .then(console.log)
+  .catch((error: any) => {
+    console.error('Search failed:', error);
+  });
