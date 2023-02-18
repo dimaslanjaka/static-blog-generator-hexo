fdeee8177352535103da07e986086b9c7d36d5da
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-18 15:06:41 +0700

    update agc
    Former-commit-id: 05617760d5248c0ce58527f372914ed684567c44

--- packages/agc/.eslintrc.js
+++ packages/agc/.eslintrc.js
@@ -1,4 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: '../../.eslintrc.js'
+  extends: '../../.eslintrc.js',
+  rules: {
+    '@typescript-eslint/adjacent-overload-signatures': 'off'
+  }
 };
--- packages/agc/src/yt-search.runner.ts
+++ packages/agc/src/yt-search.runner.ts
@@ -1,10 +1,19 @@
 import Nightmare from 'nightmare';
-const nightmare = Nightmare({ show: true });
+import path from 'upath';
+
+const nightmare = new Nightmare({
+  show: true,
+  gotoTimeout: 70000,
+  waitTimeout: 70000,
+  paths: {
+    userData: path.join(process.cwd(), 'tmp/cache/nightmare')
+  }
+});
 
 nightmare
   .goto('https://www.youtube.com/results?search_query=chimeraland+level')
   .wait('yt-formatted-string')
-  .evaluate(() => document.querySelector('yt-formatted-string').textContent)
+  .evaluate(() => Array.from(document.querySelectorAll('ytd-video-renderer')).map((el) => el.innerHTML))
   .end()
   .then(console.log)
   .catch((error: any) => {
