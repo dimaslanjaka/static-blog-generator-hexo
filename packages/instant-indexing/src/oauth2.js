6a433d007c15da35b6b736b27e4964c76f4181d7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-18 20:53:30 +0700

    update oauth2 offline access
    Former-commit-id: 9caae8d71e3d136dcdae96b2aec88007c9057d23

--- packages/instant-indexing/src/oauth2.js
+++ packages/instant-indexing/src/oauth2.js
@@ -19,18 +19,19 @@ const http = require('http');
 const url = require('url');
 const opn = require('open');
 const destroyer = require('server-destroy');
-
+const utility = require('sbg-utility');
 const { google } = require('googleapis');
 const people = google.people('v1');
 
 /**
  * To use OAuth2 authentication, we need access to a a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI.  To get these credentials for your application, visit https://console.cloud.google.com/apis/credentials.
  */
-let keys = {
+const keys = {
   redirect_uris: [process.env.GCALLBACK],
   client_id: process.env.GCLIENT,
   client_secret: process.env.GSECRET
 };
+const TOKEN_PATH = path.join(process.cwd(), '.cache/token.json');
 
 /**
  * Create a new OAuth2 client with the configured keys.
@@ -43,10 +44,46 @@ const oauth2Client = new google.auth.OAuth2(keys.client_id, keys.client_secret, 
 google.options({ auth: oauth2Client });
 
 /**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {import('googleapis').Auth.OAuth2Client} client
+ * @return {void}
+ */
+function saveCredentials(client) {
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: keys.client_id,
+    client_secret: keys.client_secret,
+    refresh_token: client.credentials.refresh_token
+  });
+  utility.writefile(TOKEN_PATH, payload);
+}
+
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {import('googleapis').Auth.OAuth2Client | null}
+ */
+function loadSavedCredentialsIfExist() {
+  try {
+    const content = fs.readFileSync(TOKEN_PATH).toString();
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
  * Open an http server to accept the oauth callback. In this simple example, the only request to our webserver is to /callback?code=<code>
  */
 async function authenticate(scopes) {
   return new Promise((resolve, reject) => {
+    const client = loadSavedCredentialsIfExist();
+    if (client) {
+      return resolve(client);
+    }
+
     // grab the url that will be used for authorization
     const authorizeUrl = oauth2Client.generateAuthUrl({
       access_type: 'offline',
@@ -55,8 +92,8 @@ async function authenticate(scopes) {
     const server = http
       .createServer(async (req, res) => {
         try {
-          if (req.url.indexOf('/oauth2callback') > -1) {
-            const qs = new url.URL(req.url, 'http://localhost:3000').searchParams;
+          if (req.url.indexOf('/auth') > -1) {
+            const qs = new url.URL(req.url, 'http://localhost:4000').searchParams;
             res.end('Authentication successful! Please return to the console.');
             server.destroy();
             const { tokens } = await oauth2Client.getToken(qs.get('code'));
@@ -67,7 +104,7 @@ async function authenticate(scopes) {
           reject(e);
         }
       })
-      .listen(3000, () => {
+      .listen(4000, () => {
         // open the browser to the authorize url to start the workflow
         opn(authorizeUrl, { wait: false }).then((cp) => cp.unref());
       });
@@ -75,7 +112,7 @@ async function authenticate(scopes) {
   });
 }
 
-async function runSample() {
+async function _getPeopleInfo() {
   // retrieve user profile
   const res = await people.people.get({
     resourceName: 'people/me',
@@ -85,10 +122,12 @@ async function runSample() {
 }
 
 const scopes = [
-  'https://www.googleapis.com/auth/contacts.readonly',
+  //'https://www.googleapis.com/auth/contacts.readonly',
   'https://www.googleapis.com/auth/user.emails.read',
-  'profile'
+  'profile',
+  'https://www.googleapis.com/auth/webmasters',
+  'https://www.googleapis.com/auth/webmasters.readonly'
 ];
 authenticate(scopes)
-  .then((client) => runSample(client))
+  .then((client) => saveCredentials(client))
   .catch(console.error);
