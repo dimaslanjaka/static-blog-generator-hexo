a149bb0f94093c921046d1489c7f02ce975f52df
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-20 00:39:30 +0700

    update submodule

--- packages/instant-indexing/src/notify.runner.ts
+++
@@ -1,7 +0,0 @@
-import { notify2 } from './notify';
-import { jwtAuthorize, saveCredentials } from './oauth2';
-
-jwtAuthorize().then((client) => {
-  saveCredentials(client);
-  notify2('https://www.webmanajemen.com/chimeraland/monsters/cicada.html', 'URL_UPDATED', client.credentials);
-});
