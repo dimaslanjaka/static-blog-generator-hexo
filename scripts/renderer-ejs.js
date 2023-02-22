25578f50ea196bc3ee95f1028e9f8f290672de96
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-23 02:01:47 +0700

    update renderer loader

--- scripts/renderer-ejs.js
+++ scripts/renderer-ejs.js
@@ -1,17 +1,23 @@
-// hexo-renderer-ejs
-/* global hexo */
 'use strict';
 
 const ejs = require('ejs');
 
-function ejsRenderer(data, locals) {
-  return ejs.render(data.text, Object.assign({ filename: data.path }, locals));
+/**
+ * hexo-renderer-ejs
+ * @param {import('hexo')} hexo
+ */
+function rendererEjs(hexo) {
+  function ejsRenderer(data, locals) {
+    return ejs.render(data.text, Object.assign({ filename: data.path }, locals));
+  }
+
+  ejsRenderer.compile = function (data) {
+    return ejs.compile(data.text, {
+      filename: data.path
+    });
+  };
+
+  hexo.extend.renderer.register('ejs', 'html', ejsRenderer, true);
 }
 
-ejsRenderer.compile = function (data) {
-  return ejs.compile(data.text, {
-    filename: data.path
-  });
-};
-
-hexo.extend.renderer.register('ejs', 'html', ejsRenderer, true);
+module.exports = { rendererEjs };
