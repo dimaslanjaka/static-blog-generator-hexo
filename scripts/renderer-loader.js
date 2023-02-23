67dd58f4e22688511d515853e3419b4183b7c44d
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-23 11:30:37 +0700

    add stylus renderer to fix hexo-theme-stun
    Former-commit-id: c3d6cc66d45baa6aa31475d8d98ff2654ddaadc9

--- scripts/renderer-loader.js
+++ scripts/renderer-loader.js
@@ -1,18 +1,20 @@
 /* global hexo */
 'use strict';
-const path = require('path');
-const fs = require('fs');
+
 const { rendererNunjucks } = require('./renderer-nunjucks');
 const { rendererEjs } = require('./renderer-ejs');
 const { rendererPug } = require('./renderer-pug');
+const { rendererStylus } = require('./renderer-stylus');
 if (typeof hexo === 'undefined') {
   global.hexo = {};
 }
 
-const themeDir = path.join(__dirname, '../themes', hexo.config.theme);
-const layoutDir = path.join(themeDir, 'layout');
-
 if (typeof hexo !== 'undefined') {
+  // const { config } = hexo;
+  // const { renderers } = config;
+  /*
+  const themeDir = path.join(__dirname, '../themes', hexo.config.theme);
+const layoutDir = path.join(themeDir, 'layout');
   if (fs.existsSync(path.join(layoutDir, '_layout.njk'))) {
     rendererNunjucks(hexo);
   } else if (fs.existsSync(path.join(layoutDir, '_layout.ejs'))) {
@@ -20,4 +22,9 @@ if (typeof hexo !== 'undefined') {
   } else if (fs.existsSync(path.join(layoutDir, '_layout.pug'))) {
     rendererPug(hexo);
   }
+  */
+  rendererNunjucks(hexo);
+  rendererEjs(hexo);
+  rendererPug(hexo);
+  rendererStylus(hexo);
 }
