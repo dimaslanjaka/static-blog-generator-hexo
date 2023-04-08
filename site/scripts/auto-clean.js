fe04b6f511d7add3134af106735ac40a4f58a5e7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-09 01:24:28 +0700

    fix: auto cleaner - add page deletion - improve existing deletion

--- site/scripts/auto-clean.js
+++ site/scripts/auto-clean.js
@@ -9,6 +9,16 @@
 const path = require('path');
 const fs = require('fs-extra');
 
+function isNumeric(str) {
+  // Check if input is string
+  if (typeof str != 'string') return false;
+
+  // Use type coercion to parse the _entirety_ of the string
+  // (`parseFloat` alone does not do this).
+  // Also ensure that the strings whitespaces fail
+  return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
 hexo.extend.filter.register('after_generate', function () {
   const base = path.join(hexo.base_dir, '.deploy_git');
   const files = [
@@ -23,10 +33,18 @@ hexo.extend.filter.register('after_generate', function () {
     'tags',
     'categories',
     'assets'
-  ].map((str) => path.join(base, str));
-  //.filter(fs.existsSync);
+  ]
+    .map((str) => path.join(base, str))
+    .filter(fs.existsSync);
   files.forEach((p) => {
-    console.log('deleting', p);
+    // console.log('deleting', p);
     if (fs.existsSync(p)) fs.rmSync(p, { force: true, recursive: true });
+  });
+
+  // clean auto generated page
+  const base_page = path.join(base, 'page');
+  fs.readdirSync(base_page).forEach((file) => {
+    const fullpath = path.join(base_page, file);
+    if (isNumeric(file)) fs.rmSync(fullpath, { force: true, recursive: true });
   });
 });
