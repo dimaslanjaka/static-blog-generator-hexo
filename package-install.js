48bdb7e20be7851b6c1fe9e0b025d0bfb06f27ff
dimaslanjaka <dimaslanjaka@gmail.com>
2023-04-08 18:10:18 +0700

    fix hexo-renderers location
    Former-commit-id: c0db95170d91e5437c491fbe22c5f4b96598a232

--- package-install.js
+++ package-install.js
@@ -22,7 +22,7 @@ const ws_dev = {
 };
 const ws_prod = {
   dependencies: {
-    'hexo-renderers': '../packages/hexo-renderers',
+    'hexo-renderers': 'https://github.com/dimaslanjaka/hexo-renderers/tarball/master',
     'hexo-shortcodes': 'https://github.com/dimaslanjaka/hexo-shortcodes/raw/master/package/release/hexo-shortcodes.tgz',
     'hexo-generator-redirect': '../packages/hexo-generator-redirect',
     'hexo-seo': 'https://github.com/dimaslanjaka/hexo-seo/tarball/master'
