42d37851d14578c6757abeaa723cf9307fc022a8
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-11 17:10:44 +0700

    feat: add token to base remote

--- site/prebuild.js
+++ site/prebuild.js
@@ -2,16 +2,18 @@ const fs = require('fs');
 const path = require('upath');
 const spawn = require('cross-spawn');
 
+const tokenBase = `https://${process.env.ACCESS_TOKEN}@github.com`
+
 const cfg = [
   {
     dest: path.join(__dirname, '.deploy_git'),
     branch: 'master',
-    remote: 'https://github.com/dimaslanjaka/dimaslanjaka.github.io.git'
+    remote: `${tokenBase}/dimaslanjaka/dimaslanjaka.github.io.git`
   },
   {
     dest: path.join(__dirname, '.deploy_git/docs'),
     branch: 'master',
-    remote: 'https://github.com/dimaslanjaka/docs.git'
+    remote: `${tokenBase}/dimaslanjaka/docs.git`
   }
 ];
 
