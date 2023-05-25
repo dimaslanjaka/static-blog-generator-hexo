69f14b53e2a732ce2eee5c983e1a586d2f6b27f2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-26 02:52:53 +0700

    feat: add custom theme layout and source

---
+++ site/prebuild.ts
@@ -0,0 +1,52 @@
+import spawn from 'cross-spawn';
+import fs from 'fs-extra';
+import Hexo from 'hexo';
+import path from 'upath';
+
+/**
+ * prepare all sources
+ */
+
+const hexo = new Hexo(__dirname, { silent: true });
+hexo.init().then(() => {
+  // copy views into theme directory
+  try {
+    fs.copySync(path.join(__dirname, 'views'), hexo.theme_dir, {
+      overwrite: true,
+      /** useful for symlink by yarn workspace */
+      dereference: true
+    });
+  } catch (e) {
+    console.log(e.message);
+  }
+});
+
+// clone deployment directory
+
+const tokenBase = `https://${process.env.ACCESS_TOKEN}@github.com`;
+
+const cfg = [
+  {
+    dest: path.join(__dirname, '.deploy_git'),
+    branch: 'master',
+    remote: `${tokenBase}/dimaslanjaka/dimaslanjaka.github.io.git`
+  },
+  {
+    dest: path.join(__dirname, '.deploy_git/docs'),
+    branch: 'master',
+    remote: `${tokenBase}/dimaslanjaka/docs.git`
+  }
+];
+
+(async () => {
+  for (let i = 0; i < cfg.length; i++) {
+    const { dest, remote, branch } = cfg[i];
+    if (!fs.existsSync(dest)) {
+      const destArg = dest.replace(path.toUnix(__dirname), '');
+      console.log('cloning', destArg);
+      await spawn.async('git', ['clone', '-b', branch, remote, destArg], {
+        cwd: __dirname
+      });
+    }
+  }
+})();
