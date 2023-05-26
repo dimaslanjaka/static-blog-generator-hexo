9edb611d8bd0109a0cc6972461d9f4664e138b97
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-26 19:42:11 +0700

    chore: moved auto clean script to prebuild script

--- site/prebuild.ts
+++ site/prebuild.ts
@@ -109,4 +109,44 @@ async function setUserEmail(options: SpawnOptions) {
   const source = path.join(base, 'github-actions');
   const dest = path.join(base, 'chimeraland', 'github-actions');
   fs.copySync(source, dest, { overwrite: true });
+
+  /**
+   * clean auto generated files inside .deploy_git
+   */
+  const files = [
+    'css',
+    'style',
+    'archives',
+    'fonts',
+    'lib',
+    'hexo-seo-js',
+    'js',
+    'hexo-shortcodes-lib',
+    'tags',
+    'categories',
+    'assets'
+  ]
+    .map((str) => path.join(base, str))
+    .filter(fs.existsSync);
+  files.forEach((p) => {
+    // console.log('deleting', p);
+    if (fs.existsSync(p)) fs.rmSync(p, { force: true, recursive: true });
+  });
+
+  // clean auto generated page
+  const base_page = path.join(base, 'page');
+  fs.readdirSync(base_page).forEach((file) => {
+    const fullpath = path.join(base_page, file);
+    if (isNumeric(file)) fs.rmSync(fullpath, { force: true, recursive: true });
+  });
 })();
+
+function isNumeric(str: string | number) {
+  const int = typeof str === 'number' ? str : parseInt(str);
+  const float = typeof str === 'number' ? str : parseFloat(str);
+
+  // Use type coercion to parse the _entirety_ of the string
+  // (`parseFloat` alone does not do this).
+  // Also ensure that the strings whitespaces fail
+  return !isNaN(int) && !isNaN(float);
+}
