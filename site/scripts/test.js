0e4f920211a078b89aa077e0357f7b99bc8d0ed9
Dimas Lanjaka <dimaslanjaka@gmail.com>
2023-07-01 18:19:58 +0700

    Update test.js

--- site/scripts/test.js
+++ site/scripts/test.js
@@ -1,2 +1,1 @@
-hexo.log.i('is contains has function', 'has' in hexo.extend.helper);
-hexo.log.i('is contains get function', 'get' in hexo.extend.helper);
+hexo.log.i('keys hexo.extend.helper', Object.keys(hexo.extend.helper));
