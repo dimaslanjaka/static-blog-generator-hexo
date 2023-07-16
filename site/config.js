4ef9910eb468f77b5db296b8700b1939eb7bcadd
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-16 12:57:58 +0700

    chore: update callback

--- site/config.js
+++ site/config.js
@@ -1,5 +1,5 @@
-const { default: extractSubmodule } = require('git-command-helper/dist/utils/extract-submodule');
-const { path, fs } = require('sbg-utility');
+const spawn = require('cross-spawn');
+const { path } = require('sbg-utility');
 
 const hexoDir = __dirname;
 const tokenBase = new URL(`https://${process.env.ACCESS_TOKEN}@github.com`);
@@ -14,8 +14,8 @@ const deployConfig = [
       console.log('remote', github.remote);
       console.log('branch', github.branch);
       // update submodule
-      // await spawn.async('git', ['submodule', 'update', '-i', '-r'], github.spawnOpt({ cwd: github.cwd }));
-      if (!fs.existsSync(path.join(github.cwd, '.gitmodules'))) {
+      await spawn.async('git', ['submodule', 'update', '-i', '-r'], github.spawnOpt({ cwd: github.cwd }));
+      /*if (!fs.existsSync(path.join(github.cwd, '.gitmodules'))) {
         const totalFiles = fs.readdirSync(path.join(github.cwd)).length;
         if (totalFiles < 10) {
           console.log('re-init');
@@ -26,16 +26,16 @@ const deployConfig = [
           await github.fetch(['origin', github.branch]);
           await github.setbranch(github.branch);
         }
-      }
+      }*/
       // pull
-      await github.pull(['--recurse-submodule']);
-      if (fs.existsSync(path.join(github.cwd, '.gitmodules'))) {
+      // await github.pull(['--recurse-submodule']);
+      /*if (fs.existsSync(path.join(github.cwd, '.gitmodules'))) {
         const submodules = extractSubmodule(path.join(github.cwd, '.gitmodules'));
         for (let i = 0; i < submodules.length; i++) {
           const submodule = submodules[i];
           console.log(submodule.cwd, submodule.branch, submodule.github?.branch);
         }
-      }
+      }*/
     }
   },
   {
