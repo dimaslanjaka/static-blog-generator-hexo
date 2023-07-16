22cc8f1a8d184369bdad2ff9e9f80097214ee59b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-16 12:20:11 +0700

    feat: add deployment config

--- site/config.js
+++ site/config.js
@@ -1,3 +1,61 @@
-const hexoDir = __dirname;
+const { spawn } = require('cross-spawn');
+const { path } = require('sbg-utility');
 
-module.exports = { hexoDir };
+const hexoDir = __dirname;
+const tokenBase = new URL(`https://${process.env.ACCESS_TOKEN}@github.com`);
+const deployConfig = [
+  {
+    dest: path.join(hexo.base_dir, '.deploy_git'),
+    branch: 'master',
+    remote: `${tokenBase}/dimaslanjaka/dimaslanjaka.github.io.git`,
+    callback: async function (github) {
+      console.log('cwd', path.toUnix(github.cwd).replace(path.toUnix(hexo.base_dir), ''));
+      console.log('remote', github.remote);
+      console.log('branch', github.branch);
+      // update submodule
+      await spawn.async('git', ['submodule', 'update', '-i', '-r'], github.spawnOpt({ cwd: github.cwd }));
+    }
+  },
+  {
+    dest: path.join(hexo.base_dir, '.deploy_git/docs'),
+    branch: 'master',
+    remote: `${tokenBase}/dimaslanjaka/docs.git`,
+    callback: async function (github) {
+      console.log('cwd', path.toUnix(github.cwd).replace(path.toUnix(hexo.base_dir), ''));
+      console.log('remote', github.remote);
+      console.log('branch', github.branch);
+    }
+  },
+  {
+    dest: path.join(hexo.base_dir, '.deploy_git/chimeraland'),
+    branch: 'gh-pages',
+    remote: `${tokenBase}/dimaslanjaka/chimeraland.git`,
+    callback: async function (github) {
+      console.log('cwd', path.toUnix(github.cwd).replace(path.toUnix(hexo.base_dir), ''));
+      console.log('remote', github.remote);
+      console.log('branch', github.branch);
+    }
+  },
+  {
+    dest: path.join(hexo.base_dir, '.deploy_git/page'),
+    branch: 'gh-pages',
+    remote: `${tokenBase}/dimaslanjaka/page.git`,
+    callback: async function (github) {
+      console.log('cwd', path.toUnix(github.cwd).replace(path.toUnix(hexo.base_dir), ''));
+      console.log('remote', github.remote);
+      console.log('branch', github.branch);
+    }
+  },
+  {
+    dest: path.join(hexo.base_dir, '.deploy_git/Web-Manajemen'),
+    branch: 'gh-pages',
+    remote: `${tokenBase}/dimaslanjaka/Web-Manajemen.git`,
+    callback: async function (github) {
+      console.log('cwd', path.toUnix(github.cwd).replace(path.toUnix(hexo.base_dir), ''));
+      console.log('remote', github.remote);
+      console.log('branch', github.branch);
+    }
+  }
+];
+
+module.exports = { hexoDir, deployConfig };
