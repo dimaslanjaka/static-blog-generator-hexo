3203cdef9b45a7f07518392a5ff4e77009b6dff6
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-16 12:35:41 +0700

    chore: split deployment initializer

--- site/prebuild.ts
+++ site/prebuild.ts
@@ -1,8 +1,5 @@
-import spawn from 'cross-spawn';
 import * as dotenv from 'dotenv';
 import fs from 'fs-extra';
-import git, { SpawnOptions } from 'git-command-helper';
-import extractSubmodule from 'git-command-helper/dist/utils/extract-submodule';
 import Hexo from 'hexo';
 import path from 'upath';
 
@@ -17,192 +14,16 @@ if (fs.existsSync(envPath)) {
 
 const hexo = new Hexo(__dirname, { silent: true });
 
-const isGithubCI =
-  typeof process.env['GITHUB_WORKFLOW'] === 'string' && typeof process.env['GITHUB_WORKFLOW_SHA'] === 'string';
-/**
- * fix git config
- * @param options
- */
-async function fixGitConfig(options: SpawnOptions) {
-  if (isGithubCI) {
-    // set username and email on github workflow
-    await spawn.spawnAsync('git', ['config', '--global', 'user.name', "'dimaslanjaka'"], options);
-    await spawn.spawnAsync('git', ['config', '--global', 'user.email', "'dimaslanjaka@gmail.com'"], options);
-    // set local user email
-    await spawn.spawnAsync('git', ['config', 'user.name', "'dimaslanjaka'"], options);
-    await spawn.spawnAsync('git', ['config', 'user.email', "'dimaslanjaka@gmail.com'"], options);
-  }
-  // set EOL LF
-  await spawn.spawnAsync('git', ['config', 'core.eol', 'lf'], options);
-  await spawn.spawnAsync('git', ['config', 'core.autocrlf', 'input'], options);
-  await spawn.spawnAsync('git', ['config', 'checkout-index', '--force', '--all'], options);
-}
-
-function killProcess(name: string) {
-  const isWin = process.platform === 'win32';
-  if (isWin) {
-    spawn.sync('taskkill', ['/f', '/im', name]);
-  } else {
-    spawn.sync('killall', [name]);
-  }
-}
-
-const tokenBase = new URL(`https://${process.env.ACCESS_TOKEN}@github.com`);
-
-const cfg = [
-  {
-    dest: path.join(hexo.base_dir, '.deploy_git'),
-    branch: 'master',
-    remote: `${tokenBase}/dimaslanjaka/dimaslanjaka.github.io.git`,
-    callback: async function (github: git) {
-      console.log('running callback');
-      console.log('remote', github.remote);
-      console.log('branch', github.branch);
-      // update submodule
-      await spawn.async('git', ['submodule', 'update', '-i', '-r'], github.spawnOpt({ cwd: github.cwd }));
-    }
-  },
-  {
-    dest: path.join(hexo.base_dir, '.deploy_git/docs'),
-    branch: 'master',
-    remote: `${tokenBase}/dimaslanjaka/docs.git`,
-    callback: async function (github: git) {
-      console.log('running callback');
-      console.log('remote', github.remote);
-      console.log('branch', github.branch);
-    }
-  },
-  {
-    dest: path.join(hexo.base_dir, '.deploy_git/chimeraland'),
-    branch: 'gh-pages',
-    remote: `${tokenBase}/dimaslanjaka/chimeraland.git`,
-    callback: async function (github: git) {
-      console.log('running callback');
-      console.log('remote', github.remote);
-      console.log('branch', github.branch);
-    }
-  },
-  {
-    dest: path.join(hexo.base_dir, '.deploy_git/page'),
-    branch: 'gh-pages',
-    remote: `${tokenBase}/dimaslanjaka/page.git`,
-    callback: async function (github: git) {
-      console.log('running callback');
-      console.log('remote', github.remote);
-      console.log('branch', github.branch);
-    }
-  },
-  {
-    dest: path.join(hexo.base_dir, '.deploy_git/Web-Manajemen'),
-    branch: 'gh-pages',
-    remote: `${tokenBase}/dimaslanjaka/Web-Manajemen.git`,
-    callback: async function (github: git) {
-      console.log('running callback');
-      console.log('remote', github.remote);
-      console.log('branch', github.branch);
-    }
-  }
-];
-
 (async function () {
   // init hexo
   await hexo.init();
   // run pre-build script
   await prebuild(hexo);
-
-  const github = new git({
-    cwd: path.join(hexo.base_dir, '.deploy_git'),
-    branch: 'master',
-    remote: `https://${process.env.ACCESS_TOKEN}@github.com/dimaslanjaka/dimaslanjaka.github.io.git`,
-    user: 'dimaslanjaka',
-    email: 'dimaslanjaka@gmail.com'
-  });
-  if (!fs.existsSync(path.join(github.cwd, '.gitmodules'))) {
-    const totalFiles = fs.readdirSync(path.join(github.cwd)).length;
-    if (totalFiles < 10) {
-      console.log('re-init');
-      await github.spawn('git', ['init']);
-      if ((await github.getremote()).push.url !== github.remote && github.remote) {
-        await github.setremote(github.remote);
-      }
-      await github.fetch(['origin', github.branch]);
-      await github.setbranch(github.branch);
-    }
-  }
-  // pull
-  await github.pull(['--recurse-submodule']);
-  if (fs.existsSync(path.join(github.cwd, '.gitmodules'))) {
-    const submodules = extractSubmodule(path.join(github.cwd, '.gitmodules'));
-    for (let i = 0; i < submodules.length; i++) {
-      const submodule = submodules[i];
-      console.log(submodule.cwd, submodule.branch, submodule.github?.branch);
-    }
-  }
 })();
 
 export async function prebuild(hexo: Hexo) {
-  await fixGitConfig({ cwd: __dirname });
-
   // copy views into theme directory
   copyViewsAsset(hexo);
-
-  // clone deployment directory
-
-  for (let i = 0; i < cfg.length; i++) {
-    const info = cfg[i];
-    if (!fs.existsSync(info.dest)) {
-      // clone
-      const destArg = info.dest.replace(path.toUnix(hexo.base_dir), '');
-      console.log('cloning', {
-        arg: destArg,
-        dest: info.dest
-      });
-      await spawn.async('git', ['clone', '-b', info.branch, info.remote, destArg], {
-        cwd: hexo.base_dir
-      });
-    }
-
-    console.log(info.dest.replace(path.toUnix(__dirname) + '/', ''), fs.existsSync(info.dest));
-    if (fs.existsSync(info.dest)) {
-      const indexLock = path.join(info.dest, '.git/index.lock');
-      if (fs.existsSync(indexLock)) {
-        killProcess('git');
-        fs.rmSync(indexLock);
-      }
-      const github = new git(info.dest);
-      await fixGitConfig({ cwd: info.dest });
-
-      const hasRemote = (
-        await spawn.async('git', ['config', 'remote.origin.url'], {
-          cwd: info.dest
-        })
-      ).output;
-      // await spawn.async('git', ['remote', 'remove', 'origin'], { cwd: info.dest,  });
-      if (hasRemote.trim() !== info.remote) {
-        console.log('update remote');
-        await spawn.async('git', ['remote', 'add', 'origin', info.remote], {
-          cwd: info.dest
-        });
-      }
-      console.log('fetching...');
-      await spawn.async('git', ['fetch'], { cwd: info.dest });
-      await spawn.async('git', ['fetch', '--all', '--prune'], {
-        cwd: info.dest
-      });
-      console.log('checkout', info.branch);
-      await spawn.async('git', ['checkout', '-f', info.branch], {
-        cwd: info.dest
-      });
-      console.log('resetting...');
-      await spawn.async('git', ['reset', '--hard', 'origin/' + info.branch], {
-        cwd: info.dest
-      });
-      if (typeof info.callback === 'function') {
-        console.log('running callback...');
-        await info.callback(github);
-      }
-    }
-  }
 
   // copy github-actions validator
   const deployDir = path.join(hexo.base_dir, '.deploy_git');
--- site/src/deployment-initializer.ts
+++ site/src/deployment-initializer.ts
@@ -1,5 +1,6 @@
+import spawn from 'cross-spawn';
 import dotenv from 'dotenv';
-import git, { spawnAsync } from 'git-command-helper';
+import git, { SpawnOptions, spawnAsync } from 'git-command-helper';
 import Hexo from 'hexo';
 import { fs, path } from 'sbg-utility';
 import { deployConfig, hexoDir } from '../config';
@@ -11,7 +12,7 @@ hexo.init().then(async () => {
   const envFile = path.join(hexo.base_dir, '.env');
   if (fs.existsSync(envFile)) dotenv.config({ path: envFile, override: true });
 
-  const config = deployConfig[0];
+  const config = deployConfig[2];
 
   const clone = !fs.existsSync(config.dest) || !fs.existsSync(path.join(config.dest, '.git'));
 
@@ -58,5 +59,48 @@ hexo.init().then(async () => {
     email: 'dimaslanjaka@gmail.com',
     branch: config.branch
   });
+
+  await fixGitConfig({ cwd: github.cwd });
+
+  const indexLock = path.join(config.dest, '.git/index.lock');
+  if (fs.existsSync(indexLock)) {
+    killProcess('git');
+    fs.rmSync(indexLock);
+  }
   await config.callback(github);
 });
+
+/**
+ * fix git config
+ * @param options
+ */
+async function fixGitConfig(options: SpawnOptions) {
+  const isGithubCI =
+    typeof process.env['GITHUB_WORKFLOW'] === 'string' && typeof process.env['GITHUB_WORKFLOW_SHA'] === 'string';
+
+  if (isGithubCI) {
+    // set username and email on github workflow
+    await spawn.spawnAsync('git', ['config', '--global', 'user.name', "'dimaslanjaka'"], options);
+    await spawn.spawnAsync('git', ['config', '--global', 'user.email', "'dimaslanjaka@gmail.com'"], options);
+    // set local user email
+    await spawn.spawnAsync('git', ['config', 'user.name', "'dimaslanjaka'"], options);
+    await spawn.spawnAsync('git', ['config', 'user.email', "'dimaslanjaka@gmail.com'"], options);
+  }
+  // set EOL LF
+  await spawn.spawnAsync('git', ['config', 'core.eol', 'lf'], options);
+  await spawn.spawnAsync('git', ['config', 'core.autocrlf', 'input'], options);
+  await spawn.spawnAsync('git', ['config', 'checkout-index', '--force', '--all'], options);
+}
+
+/**
+ * process killer by name
+ * @param name process name
+ */
+function killProcess(name: string) {
+  const isWin = process.platform === 'win32';
+  if (isWin) {
+    spawn.sync('taskkill', ['/f', '/im', name]);
+  } else {
+    spawn.sync('killall', [name]);
+  }
+}
