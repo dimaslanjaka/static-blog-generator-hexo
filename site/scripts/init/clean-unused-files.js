411953f5d1d23db6fc6bde363ff16cd8f94b28f7
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-17 16:32:08 +0700

    feat: add unused files deleter

---
+++ site/scripts/init/clean-unused-files.js
@@ -0,0 +1,13 @@
+const { path, fs } = require('sbg-utility');
+const { hexoDir } = require('../../config');
+
+hexo.extend.filter.register('after_init', function () {
+  const files = ['.frontmatter', '.github', '.vscode'].map((p) => path.join(hexoDir, 'source/_posts', p));
+
+  files.forEach((p) => {
+    if (fs.existsSync(p)) {
+      hexo.log.i('delete unused files', p);
+      fs.rmSync(p, { recursive: true, force: true });
+    }
+  });
+});
