95f26de24182bf70a3b4568e30c0ec2dab9af23f
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-17 16:18:09 +0700

    refactor: moved deployment copy into hexo event trigger inside after_generated event

---
+++ site/scripts/after_generate/deployment-copy.js
@@ -0,0 +1,12 @@
+const { path, fs } = require('sbg-utility');
+const { hexoDir } = require('../../config');
+
+hexo.extend.filter.register('after_generate', function () {
+  // copy public directory into .deploy_git
+  const deploymentDir = path.join(hexoDir, '.deploy_git');
+  const publicDir = path.join(hexoDir, 'public');
+  fs.copy(publicDir, deploymentDir, {
+    overwrite: true,
+    dereference: true
+  });
+});
