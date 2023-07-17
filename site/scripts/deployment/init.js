2ee647dd8fbc9eb9cd4f5a63b4b976f01b685128
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-17 17:41:44 +0700

    feat: add deployment initializer

---
+++ site/scripts/deployment/init.js
@@ -0,0 +1,19 @@
+const spawn = require('cross-spawn');
+const { fs, path } = require('sbg-utility');
+
+hexo.extend.filter.register('after_init', async function () {
+  if (
+    !fs.existsSync(path.join(hexo.base_dir, '.deploy_git')) ||
+    !fs.existsSync(path.join(hexo.base_dir, '.deploy_git/.git'))
+  ) {
+    await spawn.spawnAsync(
+      'git',
+      ['clone', '-b', 'master', 'https://github.com/dimaslanjaka/dimaslanjaka.github.io.git', '.deploy_git'],
+      {
+        cwd: hexo.base_dir,
+        shell: true,
+        stdio: 'inherit'
+      }
+    );
+  }
+});
