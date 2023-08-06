1eac30d3f5d879393319bc5a1aa81a733a9e49c2
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-07 03:55:12 +0700

    docs: add description

--- site/scripts/deployment/init.js
+++ site/scripts/deployment/init.js
@@ -2,6 +2,7 @@ const spawn = require('cross-spawn');
 const { fs, path } = require('sbg-utility');
 
 hexo.extend.filter.register('after_init', async function () {
+  // clone root deployment
   if (
     !fs.existsSync(path.join(hexo.base_dir, '.deploy_git')) ||
     !fs.existsSync(path.join(hexo.base_dir, '.deploy_git/.git'))
