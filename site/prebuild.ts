6d8b6ad0c4c00ac6637059d501a1fb30d60060f3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-22 22:54:53 +0700

    refactor: clean taxonomies non-git files

--- site/prebuild.ts
+++ site/prebuild.ts
@@ -1,5 +1,7 @@
+import Promise from 'bluebird';
 import * as dotenv from 'dotenv';
 import fs from 'fs-extra';
+import glob from 'glob';
 import Hexo from 'hexo';
 import path from 'upath';
 import { deployConfig } from './config';
@@ -37,29 +39,51 @@ export async function prebuild(hexo: Hexo) {
   fs.copySync(source, dest, { overwrite: true, dereference: true });
 }
 
-export function cleanAutoGenFiles(hexo: Hexo) {
+export async function cleanAutoGenFiles(hexo: Hexo) {
   const deployDir = path.join(hexo.base_dir, '.deploy_git');
 
   /**
    * clean auto generated files inside .deploy_git
    */
-  const files = ['css', 'style', 'fonts', 'lib', 'hexo-seo-js', 'js', 'hexo-shortcodes-lib', 'assets']
-    .map((str) => path.join(deployDir, str))
-    .filter(fs.existsSync);
-  files.forEach((p) => {
-    // console.log('deleting', p);
-    if (fs.existsSync(p)) fs.rmSync(p, { force: true, recursive: true });
+  await Promise.all(
+    ['css', 'style', 'fonts', 'lib', 'hexo-seo-js', 'js', 'hexo-shortcodes-lib', 'assets'].map((str) =>
+      path.join(deployDir, str)
+    )
+  )
+    .filter((file) => fs.existsSync(file))
+    .each((file) => {
+      // console.log('deleting', p);
+      fs.rmSync(file, { force: true, recursive: true });
+    });
+
+  // empty taxonomies folder without .git files
+  await Promise.all(['archives', 'categories', 'tags'].map((str) => path.join(deployDir, str))).each((base) => {
+    return Promise.all(glob.glob(['**/*.*'], { ignore: ['**/.git*'], cwd: base, posix: true }))
+      .filter((result) => {
+        if (/^[0-9]{1,4}\//.test(result)) return false;
+        if (/^node_modules\//.test(result)) return false;
+        if (/^.git\//.test(result)) return false;
+        return true;
+      })
+      .each((file) => {
+        fs.rmSync(file, { recursive: true, force: true });
+      });
   });
 
   // clean auto generated page/{n}
   const base_page = path.join(deployDir, 'page');
-  fs.readdirSync(base_page).forEach((file) => {
+  await Promise.all(fs.readdirSync(base_page)).each((file) => {
     const fullpath = path.join(base_page, file);
     if (isNumeric(file)) fs.rmSync(fullpath, { force: true, recursive: true });
   });
 }
 
-function isNumeric(str: string | number) {
+/**
+ * is string numeric
+ * @param str
+ * @returns
+ */
+function isNumeric(str: any) {
   const int = typeof str === 'number' ? str : parseInt(str);
   const float = typeof str === 'number' ? str : parseFloat(str);
 
