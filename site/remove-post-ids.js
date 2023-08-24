c53f4ad613295dff3ee1f9da01f4192fbedbbee6
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-25 01:53:26 +0700

    chore: update posts

--- site/remove-post-ids.js
+++ site/remove-post-ids.js
@@ -2,8 +2,9 @@ const glob = require('glob');
 const path = require('path');
 const Promise = require('bluebird');
 const parse = require('hexo-post-parser');
-//const moment = require('moment-timezone');
 const { writefile } = require('sbg-utility');
+
+// remove id metadata from all posts
 
 const base = path.join(__dirname, 'src-posts');
 Promise.all(glob.glob(['**/*.md'], { ignore: ['**/.*', '**/node_modules/**'], cwd: base, posix: true }))
@@ -15,7 +16,6 @@ Promise.all(glob.glob(['**/*.md'], { ignore: ['**/.*', '**/node_modules/**'], cw
   .map((str) => path.join(base, str))
   .each(async (file) => {
     try {
-      // const date = moment(new Date()).format();
       const post = await parse.parsePost(file);
       if (post.metadata.id) delete post.metadata.id;
       if (post.metadata.uuid) delete post.metadata.uuid;
