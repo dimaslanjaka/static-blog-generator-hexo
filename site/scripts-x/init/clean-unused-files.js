743b5d3e5d7be8a08e497b913a317bbf65988818
dimaslanjaka <dimaslanjaka@gmail.com>
2023-08-25 11:23:45 +0700

    export function

--- site/scripts-x/init/clean-unused-files.js
+++ site/scripts-x/init/clean-unused-files.js
@@ -1,17 +1,24 @@
 const { path, fs } = require('sbg-utility');
 const { hexoDir } = require('../../config');
+const Promise = require('bluebird');
 
 // clean unused files in source/_posts
 
-hexo.extend.filter.register('after_init', function () {
+function cleanUnusedFilesInSourcePosts() {
   const files = ['.frontmatter', '.github', '.vscode', 'tmp', 'node_modules'].map((p) =>
     path.join(hexoDir, 'source/_posts', p)
   );
 
-  files.forEach((p) => {
+  return Promise.all(files).each((p) => {
     if (fs.existsSync(p)) {
       hexo.log.i('delete unused files', p);
       fs.rmSync(p, { recursive: true, force: true });
     }
   });
-});
+}
+
+if (typeof hexo !== 'undefined') {
+  hexo.extend.filter.register('after_init', cleanUnusedFilesInSourcePosts);
+}
+
+module.exports = { cleanUnusedFilesInSourcePosts };
