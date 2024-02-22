25faa449eac3961dc9e0a3ae7f82633f29b8fe8c
dimaslanjaka <dimaslanjaka@gmail.com>
2024-02-22 22:30:27 +0700

    copy markdown files only

--- site/bin/clone-sample.cmd
+++ site/bin/clone-sample.cmd
@@ -5,3 +5,6 @@ call rimraf source/_posts/example
 call git clone https://github.com/hexojs/hexo-theme-unit-test.git source/_posts/example
 call cd source/_posts/example && git rm -rf . && git checkout origin/master source/_posts
 call cd ../../..
+call mv source/_posts/example/source/_posts/* source/_posts
+call hexo clean
+call rimraf source/_posts/example
