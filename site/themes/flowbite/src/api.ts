ca123da91d44e2e67333a8b01dc2b44ee2e593bb
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-12 04:08:11 +0700

    move flowbite folder

--- site/themes/flowbite/src/api.ts
+++
@@ -1,7 +0,0 @@
-import * as cp from "cross-spawn";
-import * as path from "upath";
-
-const cwd = path.join(__dirname, "../");
-export async function hexoThemeTailwindBuild() {
-  await cp.spawnAsync("npm", ["run", "build"], { cwd, stdio: "inherit" });
-}
