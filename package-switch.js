14087b0e679ec8910d7abee26e719c652caae0ef
dimaslanjaka <dimaslanjaka@gmail.com>
2024-10-20 13:11:13 +0700

    update hexo-post-parser

--- package-switch.js
+++ package-switch.js
@@ -1,19 +1,25 @@
 const fs = require('fs');
 const pkg = require('./package.json');
 const path = require('path');
-//D:\Repositories\static-blog-generator\packages\sbg-cli\test\post-copy.runner.ts
+const axios = require('axios').default;
+
+// node package-switch.cjs [local|production]
+
 const local = {
   'sbg-api': 'file:../sbg-api/packages/sbg-api/release/sbg-api.tgz',
   'sbg-utility': 'file:../sbg-utility/packages/sbg-utility/release/sbg-utility.tgz',
   'sbg-server': 'file:../static-blog-generator/packages/sbg-server/release/sbg-server.tgz',
   'sbg-cli': 'file:../static-blog-generator/packages/sbg-cli/release/sbg-cli.tgz',
-  'hexo-post-parser': 'file:../hexo-post-parser/release/hexo-post-parser.tgz',
   'hexo-asset-link': 'file:../hexo/releases/hexo-asset-link.tgz',
+  hexo: 'file:../hexo/releases/hexo.tgz',
   'hexo-cli': 'file:../hexo/releases/hexo-cli.tgz',
   'hexo-front-matter': 'file:../hexo/releases/hexo-front-matter.tgz',
   'hexo-log': 'file:../hexo/releases/hexo-log.tgz',
   'hexo-util': 'file:../hexo/releases/hexo-util.tgz',
-  warehouse: 'file:../hexo/releases/warehouse.tgz'
+  warehouse: 'file:../hexo/releases/warehouse.tgz',
+  'hexo-post-parser': 'file:../hexo-post-parser/release/hexo-post-parser.tgz',
+  'git-command-helper': 'file:../git-command-helper/release/git-command-helper.tgz',
+  'markdown-it': 'file:../markdown-it/release/markdown-it.tgz'
 };
 
 const production = {
@@ -31,37 +37,123 @@ const production = {
   'hexo-renderers': 'https://github.com/dimaslanjaka/hexo-renderers/raw/3f727de/release/hexo-renderers.tgz',
   'hexo-shortcodes': 'https://github.com/dimaslanjaka/hexo-shortcodes/raw/f70a1c0/release/hexo-shortcodes.tgz',
   'static-blog-generator':
-    'https://github.com/dimaslanjaka/static-blog-generator/raw/fff9b62/packages/static-blog-generator/release/static-blog-generator.tgz',
+    'https://github.com/dimaslanjaka/static-blog-generator/raw/master/packages/static-blog-generator/release/static-blog-generator.tgz',
   'instant-indexing':
-    'https://github.com/dimaslanjaka/static-blog-generator/raw/fff9b62/packages/instant-indexing/release/instant-indexing.tgz',
+    'https://github.com/dimaslanjaka/static-blog-generator/raw/master/packages/instant-indexing/release/instant-indexing.tgz',
   'sbg-utility':
-    'https://github.com/dimaslanjaka/static-blog-generator/raw/fff9b62/packages/sbg-utility/release/sbg-utility.tgz',
-  'sbg-api': 'https://github.com/dimaslanjaka/static-blog-generator/raw/fff9b62/packages/sbg-api/release/sbg-api.tgz',
-  'sbg-cli': 'https://github.com/dimaslanjaka/static-blog-generator/raw/fff9b62/packages/sbg-cli/release/sbg-cli.tgz',
+    'https://github.com/dimaslanjaka/static-blog-generator/raw/sbg-utility/packages/sbg-utility/release/sbg-utility.tgz',
+  'sbg-api': 'https://github.com/dimaslanjaka/static-blog-generator/raw/sbg-api/packages/sbg-api/release/sbg-api.tgz',
+  'sbg-cli': 'https://github.com/dimaslanjaka/static-blog-generator/raw/master/packages/sbg-cli/release/sbg-cli.tgz',
   'sbg-server':
-    'https://github.com/dimaslanjaka/static-blog-generator/raw/fff9b62/packages/sbg-server/release/sbg-server.tgz',
+    'https://github.com/dimaslanjaka/static-blog-generator/raw/master/packages/sbg-server/release/sbg-server.tgz',
   'nodejs-package-types':
     'https://github.com/dimaslanjaka/nodejs-package-types/raw/a2e797bc27975cba20ef4c87547841e6341bfcf4/release/nodejs-package-types.tgz',
-  'hexo-post-parser': 'https://github.com/dimaslanjaka/hexo-post-parser/raw/5319e01/release/hexo-post-parser.tgz',
-  'cross-spawn': 'https://github.com/dimaslanjaka/node-cross-spawn/raw/80999ac/release/cross-spawn.tgz',
-  'git-command-helper': 'https://github.com/dimaslanjaka/git-command-helper/raw/8849c22/release/git-command-helper.tgz',
+  'hexo-post-parser': 'https://github.com/dimaslanjaka/hexo-post-parser/raw/pre-release/release/hexo-post-parser.tgz',
+  'cross-spawn': 'https://github.com/dimaslanjaka/node-cross-spawn/raw/private/release/cross-spawn.tgz',
+  'git-command-helper':
+    'https://github.com/dimaslanjaka/git-command-helper/raw/pre-release/release/git-command-helper.tgz',
   '@types/git-command-helper':
-    'https://github.com/dimaslanjaka/git-command-helper/raw/8849c22/release/git-command-helper.tgz',
+    'https://github.com/dimaslanjaka/git-command-helper/raw/pre-release/release/git-command-helper.tgz',
   'hexo-generator-redirect':
     'https://github.com/dimaslanjaka/hexo-generator-redirect/raw/0885394/release/hexo-generator-redirect.tgz'
 };
 
-// node package-switch.js [local|production]
-const args = process.argv.slice(2);
+/**
+ * Fetches the latest commit from a specified GitHub repository and branch.
+ *
+ * @param {string} repoOwner - The owner of the repository.
+ * @param {string} repoName - The name of the repository.
+ * @param {string|null|undefined} [branch] - The branch to fetch the latest commit from (default is 'pre-release').
+ * @returns {Promise<Record<string, any>|null>} - The latest commit object or null if an error occurs.
+ */
+async function getLatestCommit(repoOwner, repoName, branch = '') {
+  const githubToken = process.env.ACCESS_TOKEN || process.env.GH_TOKEN;
+  let url;
+  if (typeof branch === 'string' && branch.length > 0) {
+    url = `https://api.github.com/repos/${repoOwner}/${repoName}/commits/${branch}`;
+  } else {
+    url = `https://api.github.com/repos/${repoOwner}/${repoName}/commits`;
+  }
 
-if (args.includes('local')) {
-  pkg.resolutions = Object.assign(production, local);
-} else {
-  pkg.resolutions = production;
+  try {
+    /**
+     * @type {import("axios").AxiosResponse<any, any>}
+     */
+    let response;
+    if (githubToken) {
+      response = await axios.get(url, {
+        headers: {
+          Authorization: `token ${githubToken}`,
+          Accept: 'application/vnd.github.v3+json'
+        }
+      });
+    } else {
+      response = await axios.get(url);
+    }
+    const latestCommit = response.data; // The latest commit for the specified branch
+    if (!Array.isArray(latestCommit)) {
+      return latestCommit;
+    } else {
+      return latestCommit[0];
+    }
+  } catch (error) {
+    // retry fetch default branch
+    // if (branch === "pre-release") return getLatestCommit(repoOwner, repoName, null);
+    console.error('Error fetching the latest commit:', repoOwner, repoName, branch, error.message);
+    return null;
+  }
 }
 
-pkg.resolutions = Object.fromEntries(
-  Object.entries(pkg.resolutions).sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
-);
+/**
+ * Fetches the latest commit SHA and updates the production URL for the specified repository.
+ * @param {string} repoOwner - The owner of the repository.
+ * @param {string} repoName - The name of the repository.
+ * @param {string} branch - The branch to fetch from.
+ * @param {string} packageName - The name of the package in production.
+ * @param {string} urlFormat - Custom URL format for the .tgz file.
+ */
+async function updatePackageSha(repoOwner, repoName, branch, packageName, urlFormat) {
+  const { sha } = (await getLatestCommit(repoOwner, repoName, branch)) || {};
+  if (sha) {
+    const url = urlFormat.replace('{sha}', sha);
 
-fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(pkg, null, 2) + '\n');
+    const res = await axios.get(url, { maxRedirects: 10 });
+    if (res.status === 200) {
+      production[packageName] = url;
+    }
+  }
+}
+
+async function main() {
+  const args = process.argv.slice(2);
+
+  if (args.includes('local')) {
+    pkg.resolutions = Object.assign(production, local);
+  } else {
+    // Update specific packages with their latest commit SHA
+    await updatePackageSha(
+      'dimaslanjaka',
+      'hexo-post-parser',
+      'pre-release',
+      'hexo-post-parser',
+      'https://github.com/dimaslanjaka/hexo-post-parser/raw/{sha}/release/hexo-post-parser.tgz'
+    );
+    await updatePackageSha(
+      'dimaslanjaka',
+      'markdown-it',
+      'master',
+      'markdown-it',
+      'https://github.com/dimaslanjaka/markdown-it/raw/{sha}/release/markdown-it.tgz'
+    );
+
+    pkg.resolutions = production;
+  }
+
+  pkg.resolutions = Object.fromEntries(
+    Object.entries(pkg.resolutions).sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+  );
+
+  fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(pkg, null, 2) + '\n');
+}
+
+main();
