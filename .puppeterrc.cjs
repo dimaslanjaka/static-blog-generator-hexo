65fac021651b335d5973573169de1fbee51ea675
Dimas Lanjaka <dimaslanjaka@gmail.com>
2025-08-21 06:43:23 +0700

    chore: fix puppeteer configuration by adding temporaryDirectory setting

--- .puppeterrc.cjs
+++ .puppeterrc.cjs
@@ -1,5 +1,6 @@
 // puppeteer configuration file to set custom cache and tmp directories
 module.exports = {
   cacheDirectory: 'tmp/puppeteer',
-  userDataDir: 'tmp/puppeteer/user-data'
+  userDataDir: 'tmp/puppeteer/user-data',
+  temporaryDirectory: 'tmp/puppeteer/tmp'
 };
